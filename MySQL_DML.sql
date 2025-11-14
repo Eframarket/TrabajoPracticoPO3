7880349e43fb19ea1e2f67ad085eacbc6c270d25
EfraínLlebaria <efrainllebaria@gmail.com>
2025-11-14 17:15:25 -0300

    Add files via upload

---
+++ MySQL_DML.sql
@@ -0,0 +1,38 @@
+
+use mercado;
+INSERT INTO gondolas (titulo, secciones) VALUES
+('Electrodomésticos', 'CENTRALES'),
+('Muebles', 'PARED'),
+('Ropa', 'CABECERA'),
+('Juguetes', 'ESPECIALIZADA'),
+('Deportes', 'CENTRALES'),
+('Tecnología', 'PARED'),
+('Alimentos', 'CABECERA'),
+('Cosméticos', 'ESPECIALIZADA'),
+('Libros', 'CENTRALES'),
+('Herramientas', 'PARED'),
+('Joyas', 'CABECERA'),
+('Mascotas', 'ESPECIALIZADA'),
+('Arte y Manualidades', 'CENTRALES'),
+('Cuidado Personal', 'PARED'),
+('Videojuegos', 'CABECERA'),
+('Accesorios', 'ESPECIALIZADA');
+
+INSERT INTO productos (marca, producto, codigo_gondolas) VALUES
+('Samsung', 'Lavadora AI', 1),
+('IKEA', 'Sofá Cama', 2),
+('Nike', 'Zapatillas de Correr', 3),
+('Fisher Price', 'Bloques de Construcción', 4),
+('Adidas', 'Pelota de Fútbol', 5),
+('Sony', 'Televisor 4K', 6),
+('Nestlé', 'Cereal de Maíz', 7),
+('L’Oréal', 'Shampoo Hidratante', 8),
+('HarperCollins', 'Novela de Misterio', 9),
+('Bosch', 'Taladro Inalámbrico', 10),
+('Tiffany & Co.', 'Anillo de Compromiso', 11),
+('Whiskas', 'Comida para Gatos', 12),
+('Prismacolor', 'Lápices de Colores', 13),
+('Nivea', 'Crema Hidratante', 14),
+('Nintendo', 'Consola Switch', 15),
+('Fossil', 'Reloj de Pulsera', 16),
+('EcoBags', 'Bolsa Reutilizable', 17);
